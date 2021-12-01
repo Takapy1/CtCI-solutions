d01c3117beb91f9e0bd76583658347a17e154250
Aoyama Taro <a11111111@aoyama.jp>
2021-12-01 18:39:53 +0900

    [finish] 02_Check_permutation_A.java

---
+++ Java/src/Chapter2/q2_02_Check_Permutation/QuestionA.java
@@ -0,0 +1,21 @@
+package Chapter2.q2_02_Check_Permutation;
+
+import java.util.ArrayList;
+
+public class QuestionA {
+    public static String sort_str (String str) {
+        char[] content = str.toCharArray();
+        java.util.Arrays.sort(content);
+        return new String(content);
+    }
+
+    public static boolean checkPermutation (String str1, String str2) {
+        // 文字列の長さが違うとそもそも順列になりえない
+        // sortが少し時間かかることもあり、先に順列になる可能性がないものは省いておく
+        if (str1.length() != str2.length()) return false;
+
+        String sorted1 = sort_str(str1);
+        String sorted2 = sort_str(str2);
+        return sorted1.equals(sorted2);
+    }
+}
