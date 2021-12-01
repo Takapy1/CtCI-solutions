16af3733e3f2da4aea1a155e7b28ecb6d7e59110
Aoyama Taro <a11111111@aoyama.jp>
2021-12-01 17:15:21 +0900

    [finish] q1_01_Is_Unique.rb

---
+++ ruby/chapter1/q1_01_isUnique.rb
@@ -0,0 +1,9 @@
+def unique_str?(str)
+  puts str.uniq.size - str.size == 0 ? "Unique" : "Non Unique"
+end
+
+def is_unique?(str) 
+  string.each_char.with_object({}) do |char, hash_table|
+    if hash_table
+  end
+end
